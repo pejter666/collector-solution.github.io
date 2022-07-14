9b7862a6232deff356e32371d44ef1bb8f7b0b83
collector-solution <109185937+collector-solution@users.noreply.github.com>
2022-07-15 01:07:58 +0200

    Update connection.php

--- API/connection.php
+++ API/connection.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$dbname = "id19271996_wp_da14abae1749e302c784f2418a290356";
-$username = "id19271996_wp_da14abae1749e302c784f2418a290356";
-$password = "H!))=Gi%uuABpt2q";
+$dbname = "id19271996_wp_c5979a5ab21bebeeb192f9a1e69929c0";
+$username = "id19271996_wp_c5979a5ab21bebeeb192f9a1e69929c0";
+$password = "62=[Npsi3?A&G9~c";
 try{
     $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 }catch(Exception $e){
